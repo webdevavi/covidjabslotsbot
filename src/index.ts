fe81467e74d7610edc1487354b653376ec1137a5
devavi-labs <devavilabs@gmail.com>
2021-05-31 00:55:44 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 require("module-alias/register")
 import { initTelegramBot } from "@config"
-import { PORT } from "@constants"
 import { handleMessages } from "@handler"
-import express from "express"
-
-const app = express()
 
 const bot = initTelegramBot()
+
+console.log("Bot started")
 
 // handle messages
 bot.on("message", (message) => handleMessages(bot, message))
 
-app.use("*", (_, res) =>
-  res.status(200).send("Welcome to CovidJabSlotsBot server.")
-)
-
-app.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
+bot.once("error", console.error)
