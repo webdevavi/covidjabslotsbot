2ed9dce7649c52f397c6a54fabba69e0adf6cba3
devavi-labs <devavilabs@gmail.com>
2021-05-31 00:39:46 +0530

    add: express server

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,1 +1,2 @@
 export const PROD = process.env.NODE_ENV === "production"
+export const PORT = process.env.PORT ?? 2608
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 require("module-alias/register")
 import { initTelegramBot } from "@config"
+import { PORT } from "@constants"
 import { handleMessages } from "@handler"
+import express from "express"
+
+const app = express()
 
 const bot = initTelegramBot()
 
 // handle messages
 bot.on("message", (message) => handleMessages(bot, message))
+
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
